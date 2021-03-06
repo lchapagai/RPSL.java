2a6b474e253c07889b8044dac1c06d381a4669b7
Laxmi Chapagai <laxmichapagai0@gmail.com>
2021-03-05 21:24:17 -0500

    Test passed.

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,73 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * @author laxmi chapagai
+ */
+public class RPSLSpock {
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     *
+     * @param pick this parameter is for rock,paper,scissors,lizard,spock based on it is pick by the input.
+     * @return If the pick equals null it will return false which will do not work and move to another argument.
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     *
+     * @return It has differnt case will based on what is inputed by the user. First the pick which mean there is nothing.
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     *
+     * @param c_pick is the computer pick parameter which is picked by the computer.
+     * @param h_pick is picked by the input that is choosen by the user.
+     * @return The method will return the computer picks which can be rock, paper, scissor, lizard, spock on a random pick.
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
